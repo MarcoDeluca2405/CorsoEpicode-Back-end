5312a3ad730b11ed0ac03fccdc35c43ba9c6f320
Marco De Luca <Marco_de_luca2405@hotmail.com>
2023-05-10 18:04:45 +0200

    .
    .

---
+++ eclipseWork/Week2Day3/src/Esercizio3/MainProject.java
@@ -0,0 +1,12 @@
+package Esercizio3;
+
+import org.slf4j.*;
+
+public class MainProject {
+
+	public static void main(String[] args) {
+		Logger log=LoggerFactory.getLogger(MainProject.class);
+
+	}
+
+}
