342b5b32f8d510bfd367a00408412f86fba4a903
Marco De Luca <Marco_de_luca2405@hotmail.com>
2023-05-11 10:08:51 +0200

    .
    .

--- eclipseWork/Week2Day3/src/Esercizio3/Presenza.java
+++ eclipseWork/Week2Day3/src/Esercizio3/Presenza.java
@@ -1,5 +1,57 @@
 package Esercizio3;
 
 public class Presenza {
+private String nome;
+private int giorniPresenza;
+
+
+public Presenza(String nome, int giorniPresenza) {
+	super();
+	this.nome = nome;
+	this.giorniPresenza = giorniPresenza;
+}
+
+
+/**
+ * @return the nome
+ */
+public String getNome() {
+	return nome;
+}
+
+
+/**
+ * @param nome the nome to set
+ */
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+
+/**
+ * @return the giorniPresenza
+ */
+public int getGiorniPresenza() {
+	return giorniPresenza;
+}
+
+
+/**
+ * @param giorniPresenza the giorniPresenza to set
+ */
+public void setGiorniPresenza(int giorniPresenza) {
+	this.giorniPresenza = giorniPresenza;
+}
+
+
+@Override
+public String toString() {
+	return nome+"@"+giorniPresenza;
+}
+
+
+
+
+
 
 }
---
+++ eclipseWork/Week2Day3/src/Esercizio3/RegistroPresenze.java
@@ -0,0 +1,67 @@
+package Esercizio3;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.slf4j.*;
+
+public class RegistroPresenze {
+	
+	private static List<Presenza> listPresenza=new ArrayList<Presenza>();
+	private static String path="dati/presenze.txt";
+	private static File file=new File(path);
+
+	public static void main(String[] args) {
+		Logger log=LoggerFactory.getLogger(RegistroPresenze.class);
+	
+		
+		try {
+			//creaPresenze();
+			//salvaPresenzeSuFile();
+			leggiPresenzeDaFile();
+			
+			listPresenza.forEach(p -> System.out.println(p));
+		} catch (IOException e) {
+			log.error("Errore Io file"+e.getMessage());
+			//e.printStackTrace();
+		}
+		
+	}
+
+	
+	public static void creaPresenze() {
+		listPresenza.add(new Presenza("Mario",5));
+		listPresenza.add(new Presenza("Giuseppe",9));
+		listPresenza.add(new Presenza("Francesco",7));
+	}
+	
+	public static void salvaPresenzeSuFile() throws IOException {
+		String presenzeTxt="";
+		for(Presenza presenza: listPresenza) {
+			if(presenzeTxt.length() !=0) {
+				presenzeTxt+="#";
+			}
+			presenzeTxt+=presenza;
+		}
+		FileUtils.writeStringToFile(file, presenzeTxt,"UTF-8");
+		
+	}
+	
+	public static void leggiPresenzeDaFile() throws IOException {
+		String  presenzeTxt=FileUtils.readFileToString(file,"UTF-8");
+		String [] arr=presenzeTxt.split("#");
+		
+		for(String i: arr) {
+			String [] arrPersona=i.split(" ");
+		Presenza p=new Presenza(arrPersona[0],Integer.parseInt(arrPersona[1]));
+		listPresenza.add(p);
+		}
+		
+		//System.out.println(presenzeTxt);
+		
+	}
+}
